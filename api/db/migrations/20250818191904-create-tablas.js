380abaad9945af43d963c75d8210ae12553fdbc9
Ronald-Torrens <rtacosta@hotmail.com>
2026-03-20 23:28:39 +0100

    feat: migrations fix

--- api/db/migrations/20250818191904-create-tablas.js
+++ api/db/migrations/20250818191904-create-tablas.js
@@ -11,8 +11,8 @@ const { ORDER_PRODUCT_TABLE, OrderProductSchema } = require('../models/order-pro
 
 module.exports = {
   async up (queryInterface) {
-    await queryInterface.createTable(CUSTOMER_TABLE, CustomerSchema);
     await queryInterface.createTable(USER_TABLE, UserSchema);
+    await queryInterface.createTable(CUSTOMER_TABLE, CustomerSchema);
     await queryInterface.createTable(CATEGORY_TABLE, CategorySchema);
     await queryInterface.createTable(PRODUCT_TABLE, ProductSchema);
     await queryInterface.createTable(ORDER_TABLE, OrderSchema);
@@ -20,8 +20,8 @@ module.exports = {
   },
 
   async down (queryInterface) {
-    await queryInterface.dropTable(CUSTOMER_TABLE);
     await queryInterface.dropTable(USER_TABLE);
+    await queryInterface.dropTable(CUSTOMER_TABLE);
     await queryInterface.dropTable(CATEGORY_TABLE);
     await queryInterface.dropTable(PRODUCT_TABLE);
     await queryInterface.dropTable(ORDER_TABLE);
